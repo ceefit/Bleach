8e1676492a6aa0cb25a35f3c175a7e4c4cf3f357
Amanda Duffy <duffy@valencell.com>
2019-11-12 14:07:16 -0500

    Scanning devices working

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { BleManager } from 'react-native-ble-plx'
 
 export default class App extends Component {
+
+  constructor(){
+    super();
+    this.manager = new BleManager();
+    this.state = {devices: []};
+    this.deviceIDs = [];
+  }
+
+  componentWillMount() {
+    console.log("here2");
+    const subscription = this.manager.onStateChange((state) => {
+        if (state === 'PoweredOn') {
+            this.scanAndConnect();
+            subscription.remove();
+        }
+    }, true);
+  }
+
+  scanAndConnect() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+            // Handle error (scanning will be stopped automatically)
+            console.log(error);
+            return
+        }
+
+        // Check if it is a device you are looking for based on advertisement data
+        // or other criteria.
+        // if (device.name === 'TI BLE Sensor Tag' || 
+        //     device.name === 'SensorTag') {
+            
+        //     // Stop scanning as it's not necessary if you are scanning for one device.
+        //     this.manager.stopDeviceScan();
+
+        //     // Proceed with connection.
+        // }
+
+        console.log(device.id);
+        if (device.id != null && !this.deviceIDs.includes(device.id)){
+          newDevice = [device.id, device.name]
+          devices = Array.from(this.state.devices)  // make a copy
+          devices.push(newDevice)
+          this.deviceIDs.push(device.id)
+          this.setState({devices: devices})
+        }
+    });
+  }
+
   render() {
+    text = ""
+    for (i=0; i<this.state.devices.length; i++){
+      device = this.state.devices[i]
+      deviceID = device[0]
+      deviceName = device[1]
+      text += ((deviceName == null ? "Unknown Device" : deviceName) + ": " + deviceID + "\n")
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollView>
+        <Text style={styles.header}>Devices:</Text>
+        <Text style={styles.devices}>{text}</Text>
+      </ScrollView>
     );
   }
 }
@@ -25,12 +76,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  header: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  devices: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
